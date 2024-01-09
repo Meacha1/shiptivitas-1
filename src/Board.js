5d145fce1fa4a9aebfe7d3987239be91b7600775
Meacha1 <meachattd@gmail.com>
2024-01-09 18:38:32 +0300

    react-express

--- src/Board.js
+++ src/Board.js
@@ -7,23 +7,55 @@ import './Board.css';
 export default class Board extends React.Component {
   constructor(props) {
     super(props);
-    const clients = this.getClients();
     this.state = {
       clients: {
-        backlog: clients.filter(client => !client.status || client.status === 'backlog'),
-        inProgress: clients.filter(client => client.status && client.status === 'inProgress'),
-        complete: clients.filter(client => client.status && client.status === 'complete'),
-      }
-    }
+        backlog: [],
+        inProgress: [],
+        complete: [],
+      },
+    };
     this.swimlanes = {
       backlog: React.createRef(),
       inProgress: React.createRef(),
       complete: React.createRef(),
-    }
+    };
   }
 
+
+  fetchClientsData() {
+    fetch('http://localhost:3001/api/v1/clients')
+      .then((response) => response.json())
+      .then((data) => {
+        // sort the data by priority
+        data.sort((a, b) => a.priority - b.priority);
+        const clients = data.map((clientDetails) => ({
+          id: clientDetails.id,
+          name: clientDetails.name,
+          description: clientDetails.description,
+          status: clientDetails.status,
+          priority: clientDetails.priority,
+        }));
+
+        this.setState({
+          clients: {
+            backlog: clients.filter((client) => !client.status || client.status === 'backlog'),
+            inProgress: clients.filter((client) => client.status && client.status === 'in-progress'),
+            complete: clients.filter((client) => client.status && client.status === 'complete'),
+          },
+        });
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }
+  renderSwimlane(name, clients, ref) {
+    return (
+      <Swimlane name={name} clients={clients} dragulaRef={ref}/>
+    );
+  }
   componentDidMount() {
     this.initializeDragula();
+    this.fetchClientsData();
   }
 
   initializeDragula() {
@@ -31,85 +63,57 @@ export default class Board extends React.Component {
   
     const drake = Dragula(containers);
   
-    drake.on('over', (el, container, source, target) => {
-      const closestWithDataStatus = el.closest('[data-status]');
+    drake.on('drop', (el, target) => {
+      console.log('drop - target:', target);
 
-      const swimlaneStatus = closestWithDataStatus ? closestWithDataStatus.dataset.status : null;
+      const newPriority = Array.from(target.children).findIndex(child => child === el) + 1;
+      console.log('newPriority:', newPriority);
+  
+      // Check if the target has the dataset property
+      let swimlaneStatus = null;
+      const progressTitle = target.parentElement.firstChild.textContent;
+      if (progressTitle === 'Backlog') {
+        swimlaneStatus = 'backlog';
+      } else if (progressTitle === 'In Progress') {
+        swimlaneStatus = 'in-progress';
+      } else if (progressTitle === 'Complete') {
+        swimlaneStatus = 'complete';
+      }
   
       if (swimlaneStatus) {
-        const clientId = el.dataset.id;
+        // Update the status of the dragged card
+        const cardId = el.dataset.id;
   
-        this.setState(prevState => {
-          let updatedClients = { ...prevState.clients };
-
-          const target = updatedClients[swimlaneStatus];
-
-          let index = -1
-
-          for (let i = 0; i < target.length; i++) {
-            if (target[i].id === clientId) {
-              index = i;
-              break;
-            }
-          }
-          if (index !== -1) {
-            updatedClients[swimlaneStatus][index].status = swimlaneStatus;
-          }
-          return { clients: updatedClients };
-        });
+        // Make the fetch request to update the status in the backend
+        fetch(`http://localhost:3001/api/v1/clients/${cardId}`, {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ status: swimlaneStatus, priority: newPriority }),
+        })
+          .then(response => response.json())
+          .then(updatedClient => {
+            // Update the state with the response from the backend
+            const updatedClients = this.state.clients;
+            Object.keys(updatedClients).forEach(swimlane => {
+              updatedClients[swimlane] = updatedClients[swimlane].map(client => {
+                if (client.id === cardId) {
+                  return { ...client, status: updatedClient.status };
+                }
+                return client;
+              });
+            });
+  
+            this.setState({ clients: updatedClients });
+          })
+          .catch(error => {
+            console.error('Error updating status:', error);
+          });
       }
     });
   }
-
-
-  getClients() {
-    return [
-      ['1','Stark, White and Abbott','Cloned Optimal Architecture', 'inProgress'],
-      ['2','Wiza LLC','Exclusive Bandwidth-Monitored Implementation', 'complete'],
-      ['3','Nolan LLC','Vision-Oriented 4Thgeneration Graphicaluserinterface', 'backlog'],
-      ['4','Thompson PLC','Streamlined Regional Knowledgeuser', 'inProgress'],
-      ['5','Walker-Williamson','Team-Oriented 6Thgeneration Matrix', 'inProgress'],
-      ['6','Boehm and Sons','Automated Systematic Paradigm', 'backlog'],
-      ['7','Runolfsson, Hegmann and Block','Integrated Transitional Strategy', 'backlog'],
-      ['8','Schumm-Labadie','Operative Heuristic Challenge', 'backlog'],
-      ['9','Kohler Group','Re-Contextualized Multi-Tasking Attitude', 'backlog'],
-      ['10','Romaguera Inc','Managed Foreground Toolset', 'backlog'],
-      ['11','Reilly-King','Future-Proofed Interactive Toolset', 'complete'],
-      ['12','Emard, Champlin and Runolfsdottir','Devolved Needs-Based Capability', 'backlog'],
-      ['13','Fritsch, Cronin and Wolff','Open-Source 3Rdgeneration Website', 'complete'],
-      ['14','Borer LLC','Profit-Focused Incremental Orchestration', 'backlog'],
-      ['15','Emmerich-Ankunding','User-Centric Stable Extranet', 'inProgress'],
-      ['16','Willms-Abbott','Progressive Bandwidth-Monitored Access', 'inProgress'],
-      ['17','Brekke PLC','Intuitive User-Facing Customerloyalty', 'complete'],
-      ['18','Bins, Toy and Klocko','Integrated Assymetric Software', 'backlog'],
-      ['19','Hodkiewicz-Hayes','Programmable Systematic Securedline', 'backlog'],
-      ['20','Murphy, Lang and Ferry','Organized Explicit Access', 'backlog'],
-    ].map(companyDetails => ({
-      id: companyDetails[0],
-      name: companyDetails[1],
-      description: companyDetails[2],
-      status: companyDetails[3],
-    }));
-  }
-  renderSwimlane(name, clients, ref) {
-    const color = this.getColorForSwimlane(name);
-    return (
-      <Swimlane name={name} clients={clients} dragulaRef={ref} color={color} />
-    );
-  }
-  getColorForSwimlane(name) {
-    switch (name.toLowerCase()) {
-      case 'backlog':
-        return 'grey';
-      case 'in progress':
-        return 'blue';
-      case 'complete':
-        return 'green';
-      default:
-        return '';
-    }
-  }
-
+  
   render() {
     return (
       <div className="Board">
